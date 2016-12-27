faae2d8b2515732f5e45d464e4be4b333f83b096
eoe <eoekid@yahoo.com>
2016-12-27 14:32:02 -0800

    Now standard scale X data before using it in the regression

--- logistic_regression/fit.py
+++ logistic_regression/fit.py
@@ -1,13 +1,18 @@
 import configparser
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 
 config = configparser.ConfigParser()
 config.read('settings.cfg')
 
 
 def get_forecasted_stats(X_train, Y_train, X_test):
+    scaler = StandardScaler()
     Y_train = Y_train.values.ravel()  # necessary to avoid warning
+    X_train_std = scaler.fit_transform(X_train)
+    X_test_std = scaler.transform(X_test)
+
     lr = LogisticRegression(C=1000, random_state=0)
-    lr.fit(X_train, Y_train)
-    Y_test = lr.predict(X_test)
+    lr.fit(X_train_std, Y_train)
+    Y_test = lr.predict(X_test_std)
     return Y_test
